3b9331325dd32dd8b59e196f3f139fb1b4e25ca3
KojiKuno <koji.kuno@gmail.com>
2019-07-23 10:18:04 +0900

    changed updater

--- inc/wp-github-theme-updater.php
+++ inc/wp-github-theme-updater.php
@@ -1,14 +1,23 @@
 <?php
-$updater = new Inc2734\WP_GitHub_Theme_Updater\Bootstrap( get_template(), 'Olein-jp', 'oleinpress-001' );
+$updater = new Inc2734\WP_GitHub_Theme_Updater\Bootstrap(
+	get_template(),
+	'Olein-jp',
+	'oleinpress-001'
+);
 
 add_filter(
-	'inc2734_github_theme_updater_request_url',
-	function( $url, $user_name, $repository ) {
-		if ( 'Olein-jp' === $user_name && 'oleinpress-001' === $repository ) {
-			return 'https://op.olein-design.com/op-webhooks/001/response.json';
+	'inc2734_github_theme_updater_zip_url_Olein-jp/oleinpress-001',
+	function( $url ) {
+		if ( 0 !== strpos( $url, 'https://op.olein-design.com/' ) ) {
+			return false;
 		}
 		return $url;
-	},
-	10,
-	3
+	}
+);
+
+add_filter(
+	'inc2734_github_theme_updater_request_url_Olein-jp/oleinpress-001',
+	function( $url ) {
+		return 'https://op.olein-design.com/op-webhooks/001/response.json';
+	}
 );
